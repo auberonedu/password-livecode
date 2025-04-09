7e9a39a3dbd1e45a522f6a74d118edf6a092cb36
konradkelly <konradky@gmail.com>
2025-04-09 11:10:11 -0700

    created password and printed it

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -1,5 +1,10 @@
 public class PasswordApp {
     public static void main(String[] args) {
         System.out.println("Welcome to the password checker!");
+
+        String password = "hello123";
+
+        System.out.println("The password is: " + password);
+        
     }
 }
