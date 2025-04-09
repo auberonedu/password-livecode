93dbd88c556781f9558b361cd7fe7f6997d81a02
Masih <masihrahmani87@gmail.com>
2025-04-09 11:51:35 -0700

    We add mulitiple passwords

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -5,14 +5,29 @@ public class PasswordApp {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Welcome to the password checker!");
-        String password = scan.nextLine();
+     while (scan.hasNextLine()){
 
-        System.out.println("The password is : " + password);
-        if(password.length() >= 12){
+        
+            String password = scan.nextLine();
+
+            System.out.println("The password is : " + password);
+            if(password.length() >= 12){
             System.out.println("Your password is a good lenght!");
-        }else {
+            }else {
             System.out.println("Your passowrd is too short");
-        }
+            }
 
+            char[] characters = password.toCharArray();
+
+            boolean hasSpecial = false; 
+
+            for (int i = 0; i < characters.length; i++){
+            char character = characters[i];
+            if(Character.isAlphabetic(character) == false){
+                hasSpecial = true;
+            }
+            }
+            System.out.println("Has a special character " + hasSpecial);
+            }
     }
 }
