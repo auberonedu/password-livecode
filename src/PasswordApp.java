a44a69759b6593bd268297fe3b594fbb54f5992f
Jessica H <jessicahebert75@gmail.com>
2025-04-09 11:22:02 -0700

    Hardcoded passward and printed it

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -1,5 +1,8 @@
 public class PasswordApp {
     public static void main(String[] args) {
         System.out.println("Welcome to the password checker!");
+
+        String password = "hello123";
+        System.out.println("The password is: " + password);
     }
 }
