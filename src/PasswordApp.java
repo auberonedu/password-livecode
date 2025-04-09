d5eb0c8d0889c0dd7615be41319042bc17f19f83
Jessica H <jessicahebert75@gmail.com>
2025-04-09 12:05:20 -0700

    checked to see if the user used a special char

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -6,13 +6,27 @@ public class PasswordApp {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the password checker!");
 
-        String password = scan.nextLine();
-        System.out.println("The password is: " + password);
+        while(scan.hasNext()){
 
-        if (password.length()>= 12){
-            System.out.println("Your Password is a good length!");
-        } else{
-            System.out.println("Your password is to short!!!");
+            String password = scan.nextLine();
+            System.out.println("The password is: " + password);
+
+            if (password.length()>= 12){
+                System.out.println("Your Password is a good length!");
+            } else{
+                System.out.println("Your password is to short!!!");
+            }
+            char[] characters = password.toCharArray();
+
+            boolean hasSpeacial = false;
+
+            for(int i =0; i < characters.length; i++){
+                char character = characters[i];
+                if (Character.isAlphabetic(character) == false){
+                    hasSpeacial = true;
+                }
+            }
+            System.out.println("Has a special Character " + hasSpeacial);
         }
     }
 }
