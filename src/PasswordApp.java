d266820cbadcf13f62d24cf9d097a48043766538
Kiyum-06 <kiyum1406@gmail.com>
2025-04-09 11:23:57 -0700

    user input for password

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class PasswordApp {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Welcome to the password checker!");
 
-        String password = "hello123";
+        String password = scan.nextLine();
 
         System.out.println("The password is: " + password); 
+
+        if (password.length() >= 12) {
+            System.out.println("Your password is a good length");
+        } else {
+            System.out.println("Your password is too short");
+        }
 
     }
 }
