8e994b3057e51604f7744577c1efe34f51e25bd1
Masih <masihrahmani87@gmail.com>
2025-04-09 11:25:00 -0700

    We added the lenght chacker, and user input

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class PasswordApp {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Welcome to the password checker!");
-        String password = "helllo123";
+        String password = scan.nextLine();
 
         System.out.println("The password is : " + password);
+        if(password.length() >= 12){
+            System.out.println("Your password is a good lenght!");
+        }else {
+            System.out.println("Your passowrd is too short");
+        }
 
     }
 }
