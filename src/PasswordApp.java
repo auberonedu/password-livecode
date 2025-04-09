78272afd9c8f59ae94964dd769824795b630bfd8
Iquer_Dough <206500499+DennysSaenz123@users.noreply.github.com>
2025-04-09 11:23:54 -0700

    Added scanner util to add user input for password

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -1,9 +1,20 @@
+
+import java.util.Scanner;
+
 public class PasswordApp {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the password checker!");
 
-        String password = "hello123";
+        String password = scanner.nextLine();
 
         System.out.println("The password is: " + password);
+
+        if(password.length() >= 12){
+            System.out.println("your password is a good length");
+        } else {
+            System.err.println("Your password is too short");
+        }
     }
 }
