70e192b5ef99acd7b391159bdaa8fddd7483a9a2
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-04-09 11:24:08 -0700

    added password scanner

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 public class PasswordApp {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the password checker!");
 
-        String password = "hello123";
+        String password = scan.nextLine(); //scans user input
 
         System.out.println("The password is: " + password);
+
+        if(password.length() >= 12) {
+            System.out.println("Password met characterized length");
+        }else {
+                System.out.println("Password has not met requirement(s)");
+            }
+        }
     }
-}
